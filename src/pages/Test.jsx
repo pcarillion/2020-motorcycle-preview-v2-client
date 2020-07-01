e764bf8cbbea3d52138a40c58b4102d16af21635
Paul Carillion <pcarillion@yahoo.fr>
2020-07-01 15:20:11 +0200

    api test

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+import APIHandler from './../api/APIHandler'
+
 
 const Test = () => {
+
+    const [allBikes, setAllBikes] = useState([])
+    useEffect(() => {
+        const rv = APIHandler.get("bikes/collection")
+        .then (res => 
+            {
+            console.log(res)
+            setAllBikes(res.data)})
+    }, [])
+
+
     return (
         <div>
-            
+            <ul>
+                {allBikes.map((bike, i) =>{
+                    return (<li>
+                        {bike.name}
+                    </li>)
+                } )}
+            </ul>
         </div>
     )
 }
