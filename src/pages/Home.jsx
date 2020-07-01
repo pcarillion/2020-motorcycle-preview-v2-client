a6600989e2569eec610cb3de974fcfdfd84e8118
Paul Carillion <pcarillion@yahoo.fr>
2020-07-01 16:38:50 +0200

    home finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,25 @@ import React from "react";
 
 // css
 import styles from "../styles/home.module.css";
+import '../styles/home.css'
+
+// plugins
+import { Link } from "react-router-dom"
+
 
 const Home = () => {
   return (
-    <div className={styles.videoDiv}>
-      <div className={styles.videoBlock}>
+    <div className={`${styles.videoDiv} video-div`}>
+      <div className={`${styles.videoBlock} video-block`}>
         <video preload="preload" id="video" autoplay="autoplay" loop="loop">
           <source src="./../../videoCollection3.mp4" type="video/mp4"></source>
         </video>
       </div>
+      <Link to="/collection">
+        <div className={`${styles.title} title`}>
+            2020 MOTORCYCLES PREVIEWS
+        </div>
+      </Link>
     </div>
   );
 };
