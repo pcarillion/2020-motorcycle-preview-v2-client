40771e2e443e465272e6c178eed6fced512a83c4
Paul Carillion <pcarillion@yahoo.fr>
2020-07-02 10:14:10 +0200

    new pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function Dashboard() {
+
+// css
+import '../styles/dashboard.css'
+
+// js
+import apiHandler from "../api/APIHandler";
+
+
+// plugins 
+import { Link } from "react-router-dom";
+
+const Dashboard = () => {
+
+    const [bikes, setBikes] = useState([]);
+
+    useEffect(() => {
+      apiHandler
+        .get("/bikes/collection")
+        .then((apiRes) => {
+          setBikes(apiRes.data);
+          console.log(apiRes.data);
+        })
+        .catch((apiErr) => console.log(apiErr));
+    }, []);
+  
+
     return (
-        <div>
-            <h1>Dashboard page</h1>
+        <div className="dashboard-div">
+            <h2>Dashboard</h2>
+
+            <table className="adminTable">
+                <thead className="tableTitle">
+                    <tr>
+                        <th>Brand</th>
+                        <th>Model</th>
+                        <th>Type</th>
+                        <th>Engine</th>
+                        <th>Horsepower</th>
+                        <th>Available for A2</th>
+                        <th>Price</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {bikes.map((bike, i) => {
+                        return (
+                            <tr className="admin-bikeRow">
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.brand}</Link></td>
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.name}</Link></td>
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.type}</Link></td>
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.engine}cc</Link></td>
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.horsepower}</Link></td>
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.A2? "Yes": "No"}</Link></td>
+                                <td><Link href={`/bike-${bike._id}`} target="_blank">{bike.price}€</Link></td>
+                                <td><a href="/admin/edit/{{this._id}}">EDIT</a></td>
+                                <td><a href="/admin/delete/{{this._id}}">X</a></td>
+                            </tr>
+                                )
+                        })
+                    }
+
+                </tbody>
+            </table>
         </div>
     )
 }
+
+export default Dashboard
--- src/templates/SingleBike.jsx
+++ src/templates/SingleBike.jsx
@@ -1,17 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 
 
 // plugins
 import {withRouter} from 'react-router-dom'
+import apiHandler from "../api/APIHandler";
+import { Link } from "react-router-dom";
 
+
+// css
+import './../styles/one-bike.css'
 
 const SingleBike = (props) => {
 
-    
+    const [bike, setBike] = useState([]);
+
+    useEffect(() => {
+        apiHandler
+        .get(`/bikes/one-bike-${props.match.params.id}`)
+        .then((apiRes) => {
+            setBike(apiRes.data);
+            console.log(apiRes.data)
+        })
+        .catch((apiErr) => console.log(apiErr));
+    }, []);
 
     return (
-        <div>
-            single bike page
+        <div className="one-bike-div">
+            <div className="one-bike-image">
+                <div className="one-bike-image-container">
+                    <img src={`${bike.image}`} alt="" srcset=""/>
+                </div>
+            </div>
+
+            <div className="transition"></div>
+
+            <div className="one-bike-details">
+                <div>
+                    <h2>{bike.brand} {bike.name}</h2>
+                    <p>{bike.description}</p>
+                    <table className="details-table">
+                        <tbody>
+                            <tr>
+                                <td>Bike's type</td>
+                                <td>{bike.type}</td>
+                            </tr>
+                            <tr>
+                                <td>Engine</td>
+                                <td>{bike.engine} cc</td>
+                            </tr>
+                            <tr>
+                                <td>Horsepower</td>
+                                <td>{bike.horsepower} hp</td>
+                            </tr>
+                            <tr>
+                                <td>Price</td>
+                                <td>{bike.price} €</td>
+                            </tr>
+                            <tr>
+                                <td>Available for A2</td>
+                                <td>{bike.A2}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p><Link to={`${bike.link}`}>See more on the brand's website</Link></p>
+                    {/* {{#isFavourited bike._id user.favorites}}
+                
+                    {{/isFavourited}}
+                     */}
+                    <div className="gallery">
+                        {bike.gallery && bike.gallery.map((pic, i) => {
+                            return(
+                                <img src={pic} alt="" class="photo-in-the-gallery"/>
+                            )
+                        })}}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
